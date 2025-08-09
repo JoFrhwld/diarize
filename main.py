15bdfa9b6f70d4b87670ff394b5547a7436e22d5
JoFrhwld <JoFrhwld@gmail.com>
2025-08-08 23:21:26 -0400

    logging improvements

--- main.py
+++ main.py
@@ -7,22 +7,37 @@ from pyannote.audio.pipelines.utils.hook import ProgressHook
 import torchaudio
 import pympi
 import click
+import torch
+import logging
+
+logger = logging.getLogger("diarize")
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+logger.handlers.clear()
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
 
 def load_pipeline():
+    logger.info("loading pipeline")
     load_dotenv()
     pipeline = Pipeline.from_pretrained(
         "pyannote/speaker-diarization-3.1",
         use_auth_token=os.getenv("HUGGINGFACE_ACCESS_TOKEN")
     )
-
+    if torch.cuda.is_available():
+       device = torch.device("cuda")
+       logger.info("running on cuda")
+    else:
+       device = torch.device("cpu")
+       logger.info("running on cpu")
+    pipeline.to(device)
     return pipeline
 
 
 def do_diarize(path:Path|str, min_speakers:int = 2, max_speakers:int = 4)->Annotation:
-  print("loading pipeline")
-
   pipeline = load_pipeline()
-  print("beginning diarization")
+  logger.info("starting diarization")
   waveform, sample_rate = torchaudio.load(str(path))
   waveform = torchaudio.functional.resample(
     waveform, 
@@ -42,6 +57,7 @@ def do_diarize(path:Path|str, min_speakers:int = 2, max_speakers:int = 4)->Annot
 
 def write_eaf(diarization:Annotation, out_path:Path|str, audio_file: Path|str) -> None:
   eaf = pympi.Eaf()
+  logger.info("writing diarization")
   for turn, _, speaker in diarization.itertracks(yield_label=True):
     if not speaker in eaf.get_tier_names():
       eaf.add_tier(tier_id = speaker)
@@ -62,12 +78,14 @@ def write_eaf(diarization:Annotation, out_path:Path|str, audio_file: Path|str) -
    type = click.Path(path_type=Path)
 )
 def main(path:Path|str):
+    logging.basicConfig(level=logging.INFO)
     diarization = do_diarize(path)
     write_eaf(
        diarization=diarization,
        out_path=Path(path).with_suffix(".eaf"),
        audio_file=path
     )
+    logger.info("job finished")
 
 
 if __name__ == "__main__":
